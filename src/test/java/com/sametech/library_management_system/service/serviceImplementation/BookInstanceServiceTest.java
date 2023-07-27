7c3a19052e043907a3c9de9e7dee31b74ae2cf6c
Kinsammy <fanusamuel@gmail.com>
2023-07-27 13:07:59 +0100

    Borrow book and Return book on progress

--- src/test/java/com/sametech/library_management_system/service/serviceImplementation/BookInstanceServiceTest.java
+++ src/test/java/com/sametech/library_management_system/service/serviceImplementation/BookInstanceServiceTest.java
@@ -27,7 +27,9 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@Transactional
 class BookInstanceServiceTest {
 
     @Autowired
